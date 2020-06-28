e3b504ba174315b9e5601da8fe47e4190c5380aa
Jose A. Alonso <JoseA.Alonso@gmail.com>
2020-06-28 20:45:41 +0200

    La táctica intros

--- src/Logica/La_tactica_intros.lean
+++ src/Logica/La_tactica_intros.lean
@@ -1,22 +1,25 @@
-import data.real.basic
+import data.real.basic tactic
 
 lemma my_lemma : ∀ {x y ε : ℝ},
   0 < ε → ε ≤ 1 → abs x < ε → abs y < ε → abs (x * y) < ε :=
 begin
   intros x y ε epos ele1 xlt ylt,
-  calc
-    abs (x * y) = abs x * abs y : by rw abs_mul
-            ... ≤ abs x * ε     : by rw [mul_le_mul_left' ylt]
-            ... < ε * ε         : sorry
-            ... ≤ 1 * ε         : sorry
-            ... = ε             : by rw [one_mul]
+  by_cases (abs x = 0),
+  { calc abs (x * y) = abs x * abs y : by apply abs_mul
+                 ... = 0 * abs y     : by rw h
+                 ... = 0             : by apply zero_mul
+                 ... < ε             : by apply epos
+  },
+  { have h1 : 0 < abs x,
+      { have h2 : 0 ≤ abs x,
+          apply abs_nonneg,
+        exact lt_of_le_of_ne h2 (ne.symm h)
+      },
+    calc
+      abs (x * y) = abs x * abs y : by rw abs_mul
+              ... < abs x * ε     : by apply (mul_lt_mul_left h1).mpr ylt
+              ... < ε * ε         : by apply (mul_lt_mul_right epos).mpr xlt
+              ... ≤ 1 * ε         : by apply (mul_le_mul_right epos).mpr ele1
+              ... = ε             : by rw [one_mul]
+  },
 end
-
--- Lemas usados
--- ============
-
-#check (abs_mul : ∀ (a b : ℝ), abs (a * b) = abs a * abs b)
-#check (mul_le_mul : a ≤ c → b ≤ d → 0 ≤ b → 0 ≤ c → a * b ≤ c * d)
-#check (abs_nonneg : ∀ (a : ℝ), 0 ≤ abs a)
-#check (mul_lt_mul_right : 0 < c → (a * c < b * c ↔ a < b))
-#check (one_mul : ∀ (a : ℝ), 1 * a = a)
