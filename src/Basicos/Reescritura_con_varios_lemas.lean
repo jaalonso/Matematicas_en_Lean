3edbc1a41a84250155473822f94a03ce4577dc1c
Jose A. Alonso <JoseA.Alonso@gmail.com>
2020-07-03 12:16:19 +0200

    Reescritura con varios lemas

--- src/Basicos/Reescritura_con_varios_lemas.lean
+++ src/Basicos/Reescritura_con_varios_lemas.lean
@@ -1,15 +1,90 @@
+-- ---------------------------------------------------------------------
+-- Ejercicio. Demostrar que si a, b, c, d, e y f son números reales
+-- tales que 
+--    a * b = c * d
+--    e = f
+-- entonces
+--    a * (b * e) = c * (d * f)
+-- ---------------------------------------------------------------------
+
 import data.real.basic
 
 -- 1ª demostración
-example (a b c d e f : ℝ) (h : a * b = c * d) (h' : e = f) :
-  a * (b * e) = c * (d * f) :=
+-- ===============
+
+example 
+  (a b c d e f : ℝ) 
+  (h1 : a * b = c * d) 
+  (h2 : e = f) 
+  : a * (b * e) = c * (d * f) :=
 begin
-  rw [h', ←mul_assoc, h, mul_assoc]
+  rw h2, 
+  rw ←mul_assoc, 
+  rw h1, 
+  rw mul_assoc,
 end
 
--- 2ª demostración
-example (a b c d e f : ℝ) (h : a * b = c * d) (h' : e = f) :
-  a * (b * e) = c * (d * f) :=
-by rw [h', ←mul_assoc, h, mul_assoc]
+-- Desarrollo de la prueba
+--
+--    a b c d e f : ℝ,
+--    h1 : a * b = c * d,
+--    h2 : e = f
+--    ⊢ a * (b * e) = c * (d * f)
+-- rw h2, 
+--    ⊢ a * (b * f) = c * (d * f)
+-- rw ←mul_assoc, 
+--    ⊢ (a * b) * f = c * (d * f)
+-- rw h1, 
+--    ⊢ (c * d) * f = c * (d * f)
+-- rw mul_assoc,
+--    no goals 
 
--- Nota: Colocando el cursor en las comas se observa el progreso en "Lean Goal" 
+-- 2ª demostración
+-- ===============
+
+example 
+  (a b c d e f : ℝ) 
+  (h1 : a * b = c * d) 
+  (h2 : e = f) 
+  : a * (b * e) = c * (d * f) :=
+begin
+  rw [h2, ←mul_assoc, h1, mul_assoc]
+end
+
+-- Comentario: Colocando el cursor en las comas se observa el progreso
+-- en la ventana de objetivos.
+
+-- 3ª demostración
+-- ===============
+
+example 
+  (a b c d e f : ℝ) 
+  (h1 : a * b = c * d) 
+  (h2 : e = f) 
+  : a * (b * e) = c * (d * f) :=
+by rw [h2, ←mul_assoc, h1, mul_assoc]
+
+-- 4ª demostración
+-- ===============
+
+example 
+  (a b c d e f : ℝ) 
+  (h1 : a * b = c * d) 
+  (h2 : e = f) 
+  : a * (b * e) = c * (d * f) :=
+by finish
+
+-- 5ª demostración
+-- ===============
+
+example 
+  (a b c d e f : ℝ) 
+  (h1 : a * b = c * d) 
+  (h2 : e = f) 
+  : a * (b * e) = c * (d * f) :=
+calc
+  a * (b * e) = a * (b * f) : by rw h2 
+          ... = (a * b) * f : by rw ←mul_assoc
+          ... = (c * d) * f : by rw h1
+          ... = c * (d * f) : by rw mul_assoc
+
