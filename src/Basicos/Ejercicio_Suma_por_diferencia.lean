32db324795c721d694f2b38e4e7fae092c991834
Jose A. Alonso <JoseA.Alonso@gmail.com>
2020-07-04 17:13:08 +0200

    Ejercicio: Suma por diferencia

--- src/Basicos/Ejercicio_Suma_por_diferencia.lean
+++ src/Basicos/Ejercicio_Suma_por_diferencia.lean
@@ -1,31 +1,64 @@
+-- ---------------------------------------------------------------------
+-- Ejercicio. Demostrar que si a b d son números reales, entonces
+--    (a + b) * (a - b) = a^2 - b^2 
+-- ---------------------------------------------------------------------
+
 import data.real.basic
 
 variables a b c d : ℝ
 
-#check pow_two a
--- | pow_two a : a ^ 2 = a * a 
+-- 1ª demostración (por reescritura usando el lema anterior)
+-- =========================================================
 
-#check mul_sub a b c
--- | mul_sub a b c : a * (b - c) = a * b - a * c 
-
-#check add_mul a b c
--- | add_mul a b c : (a + b) * c = a * c + b * c 
-
-#check add_sub a b c
--- | add_sub a b c : a + (b - c) = a + b - c
-
-#check sub_sub a b c
--- | sub_sub a b c : a - b - c = a - (b + c)
-
-#check add_zero a
--- | add_zero a : a + 0 = a
-
--- 1ª demostración
-example (a b : ℝ) : (a + b) * (a - b) = a^2 - b^2 :=
+-- El lema anterior es
+lemma aux : (a + b) * (c + d) = a * c + a * d + b * c + b * d :=
 by ring
 
--- 2ª demostración
-example (a b : ℝ) : (a + b) * (a - b) = a^2 - b^2 :=
+-- La demostración es
+example : (a + b) * (a - b) = a^2 - b^2 :=
+begin
+  rw sub_eq_add_neg,
+  rw aux,
+  rw mul_neg_eq_neg_mul_symm,
+  rw add_assoc (a * a),
+  rw mul_comm b a,
+  rw neg_add_self,
+  rw add_zero,
+  rw ← pow_two,
+  rw mul_neg_eq_neg_mul_symm,
+  rw ← pow_two,
+  rw ← sub_eq_add_neg,
+end
+
+-- El desarrollo de la demostración es
+--    ⊢ (a + b) * (a - b) = a ^ 2 - b ^ 2
+-- rw sub_eq_add_neg,
+--    ⊢ (a + b) * (a + -b) = a ^ 2 - b ^ 2
+-- rw aux,
+--    ⊢ a * a + a * -b + b * a + b * -b = a ^ 2 - b ^ 2
+-- rw mul_neg_eq_neg_mul_symm,
+--    ⊢ a * a + -(a * b) + b * a + b * -b = a ^ 2 - b ^ 2
+-- rw add_assoc (a * a),
+--    ⊢ a * a + (-(a * b) + b * a) + b * -b = a ^ 2 - b ^ 2
+-- rw mul_comm b a,
+--    ⊢ a * a + (-(a * b) + a * b) + b * -b = a ^ 2 - b ^ 2
+-- rw neg_add_self,
+--    ⊢ a * a + 0 + b * -b = a ^ 2 - b ^ 2
+-- rw add_zero,
+--    ⊢ a * a + b * -b = a ^ 2 - b ^ 2
+-- rw ← pow_two,
+--    ⊢ a ^ 2 + b * -b = a ^ 2 - b ^ 2
+-- rw mul_neg_eq_neg_mul_symm,
+--    ⊢ a ^ 2 + -(b * b) = a ^ 2 - b ^ 2
+-- rw ← pow_two,
+--    ⊢ a ^ 2 + -b ^ 2 = a ^ 2 - b ^ 2
+-- rw ← sub_eq_add_neg,
+--    no goals
+
+-- 3ª demostración (por calc)
+-- ==========================
+
+example : (a + b) * (a - b) = a^2 - b^2 :=
 calc
   (a + b) * (a - b)
       = a * (a - b) + b * (a - b)         : by rw add_mul
@@ -43,5 +76,19 @@ calc
   ... = (a^2 + 0) + -b^2                  : by rw ← add_assoc 
   ... = a^2 + -b^2                        : by rw add_zero
   ... = a^2 - b^2                         : by exact rfl
+
+-- Comentario. Se han usado los siguientes lemas:
+-- + pow_two a : a ^ 2 = a * a 
+-- + mul_sub a b c : a * (b - c) = a * b - a * c 
+-- + add_mul a b c : (a + b) * c = a * c + b * c 
+-- + add_sub a b c : a + (b - c) = a + b - c
+-- + sub_sub a b c : a - b - c = a - (b + c)
+-- + add_zero a : a + 0 = a
+
+-- 4ª demostración
+-- ===============
+
+example : (a + b) * (a - b) = a^2 - b^2 :=
+by ring
 
 
