9aa01a751ca3e7e2806c5614b1bdcaa74e98a891
Jose A. Alonso <JoseA.Alonso@gmail.com>
2020-07-06 12:48:51 +0200

    Las tácticas apply y exact

--- src/Basicos/Las_tacticas_apply_y_exact.lean
+++ src/Basicos/Las_tacticas_apply_y_exact.lean
@@ -1,29 +1,101 @@
+-- ---------------------------------------------------------------------
+-- Ejercicio 1. Realizar las siguientes acciones:
+--    1. Importar la teoría de los números reales
+--    2. Declarar x, y y z como variables sobre R. 
+-- ----------------------------------------------------------------------
+
 import data.real.basic
 
-example (x y z : ℝ) (h₀ : x ≤ y) (h₁ : y ≤ z) : x ≤ z :=
+variables (x y z : ℝ)
+
+-- ---------------------------------------------------------------------
+-- Ejercicio 2. Demostrar que si
+--    x ≤ y
+--    y ≤ z
+-- entonces 
+--    x ≤ z 
+-- ----------------------------------------------------------------------
+
+-- 1ª demostración
+-- ===============
+
+example 
+  (h1 : x ≤ y) 
+  (h2 : y ≤ z) 
+  : x ≤ z :=
 begin
   apply le_trans,
-  { apply h₀ },
-  apply h₁
+  { apply h1, },
+  apply h2,
 end
 
-example (x y z : ℝ) (h₀ : x ≤ y) (h₁ : y ≤ z) : x ≤ z :=
+-- El desarrollo de la prueba es
+--
+--    x y z : ℝ,
+--    h1 : x ≤ y,
+--    h2 : y ≤ z
+--    ⊢ x ≤ z
+-- apply le_trans,
+--    ⊢ x ≤ ?m_1
+-- { apply h1 },
+--    ⊢ y ≤ z
+-- apply h2,
+--    no goals
+
+-- 2ª demostración
+-- ===============
+
+example 
+  (h1 : x ≤ y) 
+  (h2 : y ≤ z) 
+  : x ≤ z :=
 begin
-  apply le_trans h₀,
-  apply h₁
+  apply le_trans h1,
+  apply h2,
 end
 
-example (x y z : ℝ) (h₀ : x ≤ y) (h₁ : y ≤ z) : x ≤ z :=
-by exact le_trans h₀ h₁
+-- El desarrollo de la prueba es
+--
+--    x y z : ℝ,
+--    h1 : x ≤ y,
+--    h2 : y ≤ z
+--    ⊢ x ≤ z
+-- apply le_trans h1,
+--    ⊢ y ≤ z
+-- apply h2,
+--    no goals
 
-example (x y z : ℝ) (h₀ : x ≤ y) (h₁ : y ≤ z) : x ≤ z :=
-le_trans h₀ h₁
+-- 3ª demostración
+-- ===============
 
-example (x : ℝ) : x ≤ x :=
+example 
+  (h1 : x ≤ y) 
+  (h2 : y ≤ z) 
+  : x ≤ z :=
+by exact le_trans h1 h2
+
+-- 4ª demostración
+-- ===============
+
+example 
+  (h1 : x ≤ y) 
+  (h2 : y ≤ z) 
+  : x ≤ z :=
+le_trans h1 h2
+
+-- ---------------------------------------------------------------------
+-- Ejercicio 4. Demostrar que
+--    x ≤ x
+-- ----------------------------------------------------------------------
+
+-- 1ª demostración
+example : x ≤ x :=
 by apply le_refl
 
-example (x : ℝ) : x ≤ x :=
+-- 2ª demostración
+example : x ≤ x :=
 by exact le_refl x
 
+-- 3ª demostración
 example (x : ℝ) : x ≤ x :=
 le_refl x
