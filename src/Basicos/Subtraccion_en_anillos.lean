8b9603c0966bcacea6d3d600b65e1b3d696f0560
Jose A. Alonso <JoseA.Alonso@gmail.com>
2020-07-06 11:54:52 +0200

    Subtracción en anillos

--- src/Basicos/Subtraccion_en_anillos.lean
+++ src/Basicos/Subtraccion_en_anillos.lean
@@ -1,13 +1,27 @@
-import algebra.ring
+-- ---------------------------------------------------------------------
+-- Ejercicio 1. Realizar las siguientes acciones:
+--    1. Importar la teoría de anillos.
+--    2. Crear el espacio de nombres my_ring
+--    3. Declarar R como una variable sobre anillos.
+--    4. Declarar a y b como variables sobre R. 
+-- ----------------------------------------------------------------------
 
-namespace my_ring
+import algebra.ring            -- 1
+namespace my_ring              -- 2
+variables {R : Type*} [ring R] -- 3
+variables (a b : R)            -- 4
 
-variables {R : Type*} [ring R]
+-- ---------------------------------------------------------------------
+-- Ejercicio 2. Demostrar que
+--    a - b = a + -b
+-- ----------------------------------------------------------------------
 
-theorem sub_eq_add_neg (a b : R) : a - b = a + -b :=
+-- 1ª demostración
+theorem sub_eq_add_neg  : a - b = a + -b :=
 rfl
 
-example (a b : R) : a - b = a + -b :=
+-- 2ª demostración
+example : a - b = a + -b :=
 by reflexivity
 
 end my_ring
