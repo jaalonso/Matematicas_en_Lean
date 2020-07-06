35914eb3c75d54e7fb60e6277bc2db17f6108be3
Jose A. Alonso <JoseA.Alonso@gmail.com>
2020-07-06 21:18:12 +0200

    Ejercicio con library_search

--- src/Basicos/Ejercicio_con_library_search.lean
+++ src/Basicos/Ejercicio_con_library_search.lean
@@ -1,3 +1,10 @@
+-- ---------------------------------------------------------------------
+-- Ejercicio. Sean a, b y c números reales. Demostrar que si
+--    a ≤ b
+-- entonces
+--    c - exp b ≤ c - exp a :=
+-- ----------------------------------------------------------------------
+
 import import analysis.special_functions.exp_log
 import tactic
 
@@ -6,7 +13,11 @@ open real
 variables a b c : ℝ
 
 -- 1ª demostración
-example (h : a ≤ b) : c - exp b ≤ c - exp a :=
+-- ===============
+
+example 
+  (h : a ≤ b) 
+  : c - exp b ≤ c - exp a :=
 begin
    apply add_le_add,
    apply le_refl,
@@ -14,8 +25,35 @@ begin
    apply exp_le_exp.mpr h,
 end
 
+-- El desarrollo de la prueba es
+-- 
+--    a b c : ℝ,
+--    h : a ≤ b
+--    ⊢ c - b.exp ≤ c - a.exp
+-- apply add_le_add,
+-- |    ⊢ c ≤ c
+-- | apply le_refl,
+--    ⊢ -b.exp ≤ -a.exp
+-- apply neg_le_neg,
+--    ⊢ a.exp ≤ b.exp
+-- apply exp_le_exp.mpr h,
+--    no goals
+
 -- 2ª demostración
-example (h : a ≤ b) : c - exp b ≤ c - exp a :=
+-- ===============
+
+example 
+  (h : a ≤ b) 
+  : c - exp b ≤ c - exp a :=
+-- by library_search [exp_le_exp.mpr h] 
+by exact sub_le_sub_left (exp_le_exp.mpr h) c
+
+-- 3ª demostración
+-- ===============
+
+example 
+  (h : a ≤ b) 
+  : c - exp b ≤ c - exp a :=
 by linarith [exp_le_exp.mpr h]
 
 -- Los lemas usados son:
