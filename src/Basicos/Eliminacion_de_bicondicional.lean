834bec8da7a8cc89dfb1e495be8022421a565775
Jose A. Alonso <JoseA.Alonso@gmail.com>
2020-07-06 18:03:02 +0200

    Eliminación de bicondicional

--- src/Basicos/Eliminacion_de_bicondicional.lean
+++ src/Basicos/Eliminacion_de_bicondicional.lean
@@ -1,22 +1,60 @@
-import analysis.special_functions.exp_log
+-- ---------------------------------------------------------------------
+-- Ejercicio 1. Realizar las siguientes acciones
+--    1. Inportar la teoría de exponeciales y logaritmos.
+--    2. Abrir la teoría de los reales
+--    3. Declarar a, b, c, d y e como variables sobre los reales.
+-- ----------------------------------------------------------------------
 
-open real
+import analysis.special_functions.exp_log   -- 1
+open real                                   -- 2
+variables a b c d e : ℝ                     -- 3 
 
-variables a b c d e : ℝ
+-- ---------------------------------------------------------------------
+-- Ejercicio 2. Calcular el tipo de los siguientes lemas 
+--    add_lt_add_of_le_of_lt
+--    exp_lt_exp 
+--    le_refl 
+-- ----------------------------------------------------------------------
 
-#check (add_lt_add_of_le_of_lt : a ≤ b → c < d → a + c < b + d)
-#check (exp_lt_exp : exp a < exp b ↔ a < b)
-#check (le_refl : ∀ (a : real), a ≤ a)
+#check @add_lt_add_of_le_of_lt ℝ _ a b c d 
+#check @exp_lt_exp a b
+#check le_refl 
 
-example (h₀ : a ≤ b) (h₁ : c < d) : a + exp c + e < b + exp d + e :=
+-- Comentario: Colocando el cursor sobre check se obtiene
+--    add_lt_add_of_le_of_lt : a ≤ b → c < d → a + c < b + d 
+--    exp_lt_exp : a.exp < b.exp ↔ a < b 
+--    le_refl : ∀ (a : ?M_1), a ≤ a  
+
+-- ---------------------------------------------------------------------
+-- Ejercicio 3. Demostrar que si
+--    a ≤ b 
+--    c < d
+-- entonces
+--    a + exp c + e < b + exp d + e :=
+-- ----------------------------------------------------------------------
+
+example 
+  (h₀ : a ≤ b) 
+  (h₁ : c < d) 
+  : a + exp c + e < b + exp d + e :=
 begin
   apply add_lt_add_of_lt_of_le,
   { apply add_lt_add_of_le_of_lt h₀,
-    apply exp_lt_exp.mpr h₁ },
+    apply exp_lt_exp.mpr h₁, },
   apply le_refl
 end
 
--- Nota: Con mpr se indica en modus pones inverso. Por ejemplo, si
--- h: A ↔ B, entonces h.mpr es B → A y h.mp es A → B
-
+-- El desarrollo de la prueba es
+-- 
+--    a b c d e : ℝ,
+--    h₀ : a ≤ b,
+--    h₁ : c < d
+--    ⊢ a + c.exp + e < b + d.exp + e
+-- apply add_lt_add_of_lt_of_le,
+-- | { apply add_lt_add_of_le_of_lt h₀,
+-- |      ⊢ a + c.exp < b + d.exp
+-- |   apply exp_lt_exp.mpr h₁ },
+--    ⊢ e ≤ e 
+-- apply le_refl
+--    no_goals
  
