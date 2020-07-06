b1b3f0429cb437c0e6754612b1d5fe353e34154d
Jose A. Alonso <JoseA.Alonso@gmail.com>
2020-07-06 12:17:04 +0200

    Axiomas de grupo

--- src/Basicos/Axiomas_de_grupo.lean
+++ src/Basicos/Axiomas_de_grupo.lean
@@ -1,7 +1,20 @@
+-- ---------------------------------------------------------------------
+-- Ejercicio 1. Imprtar la librería de grupos 
+-- ----------------------------------------------------------------------
+
+
 import algebra.group
+
+-- ---------------------------------------------------------------------
+-- Ejercicio 2. Declarar A como un tipo sobre grupos aditivos. 
+-- ----------------------------------------------------------------------
 
 variables (A : Type*) [add_group A]
 
-#check (add_assoc : ∀ a b c : A, a + b + c = a + (b + c))
-#check (zero_add : ∀ a : A, 0 + a = a)
-#check (add_left_neg : ∀ a : A, -a + a = 0)
+-- ---------------------------------------------------------------------
+-- Ejercicio 3. Comprobar que A verifica los axiomas de los grupos 
+-- ----------------------------------------------------------------------
+
+#check (add_assoc :    ∀ a b c : A, a + b + c = a + (b + c))
+#check (zero_add :     ∀ a : A,     0 + a = a)
+#check (add_left_neg : ∀ a : A,     -a + a = 0)
