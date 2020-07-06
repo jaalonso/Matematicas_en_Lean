84873d7f43d45e927673d97f92a0518287aa6528
Jose A. Alonso <JoseA.Alonso@gmail.com>
2020-07-06 11:02:08 +0200

    Lema mul_zero con have

--- src/Basicos/zero_mul.lean
+++ src/Basicos/zero_mul.lean
@@ -1,10 +1,17 @@
+-- ---------------------------------------------------------------------
+-- Ejercicio. Demostrar que en los anillos,
+--    0 * a = 0 
+-- ----------------------------------------------------------------------
+
 import algebra.ring
 
 namespace my_ring
 
 variables {R : Type*} [ring R]
 
-theorem zero_mul (a : R) : 0 * a = 0 :=
+variable (a : R)
+
+theorem zero_mul : 0 * a = 0 :=
 have 0 * a + 0 = 0 * a + 0 * a, from calc
   0 * a + 0 = (0 + 0) * a   : by simp
         ... = 0 * a + 0 * a : by rw add_mul,
