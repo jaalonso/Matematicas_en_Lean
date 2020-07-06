99a6559529a0a14637a46538e833b66252f59fd6
Jose A. Alonso <JoseA.Alonso@gmail.com>
2020-07-06 11:09:46 +0200

    Ejercicio zero_mul

--- src/Basicos/mul_zero.lean
+++ src/Basicos/mul_zero.lean
@@ -1,13 +1,35 @@
+-- ---------------------------------------------------------------------
+-- Ejercicio. Demostrar que en los anillos
+--    a * 0 = 0
+-- ----------------------------------------------------------------------
+
 import algebra.ring
 
 namespace my_ring
 
 variables {R : Type*} [ring R]
 
-theorem mul_zero (a : R) : a * 0 = 0 :=
+variable (a : R) 
+
+-- 1ª demostración
+-- ===============
+
+theorem mul_zero : a * 0 = 0 :=
 begin
   have h : a * 0 + a * 0 = a * 0 + 0,
   { rw [←mul_add, add_zero, add_zero] },
+  rw add_left_cancel h
+end
+
+-- 2ª demostración
+-- ===============
+
+example : a * 0 = 0 :=
+begin
+  have h : a * 0 + a * 0 = a * 0 + 0,
+    calc a * 0 + a * 0 = a * (0 + 0) : by rw ←mul_add
+                   ... = a * 0       : by rw add_zero
+                   ... = a * 0 + 0   : by rw add_zero,
   rw add_left_cancel h
 end
 
