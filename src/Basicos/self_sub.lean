d0382fc9f4a6ac6da8a26ba3b1577125b2137a41
Jose A. Alonso <JoseA.Alonso@gmail.com>
2020-07-06 12:11:08 +0200

    Ejercicio self_sub

--- src/Basicos/self_sub.lean
+++ src/Basicos/self_sub.lean
@@ -1,10 +1,23 @@
-import algebra.ring
+-- ---------------------------------------------------------------------
+-- Ejercicio 1. Realizar las siguientes acciones:
+--    1. Importar la teoría de anillos.
+--    2. Crear el espacio de nombres my_ring
+--    3. Declarar R como una variable sobre anillos.
+--    4. Declarar a como variable sobre R. 
+-- ----------------------------------------------------------------------
 
-namespace my_ring
+import algebra.ring            -- 1
+namespace my_ring              -- 2
+variables {R : Type*} [ring R] -- 3
+variables (a : R)              -- 4
 
-variables {R : Type*} [ring R]
+-- ---------------------------------------------------------------------
+-- Ejercicio 2. Demostrar que
+--     a - a = 0
+-- ----------------------------------------------------------------------
 
-theorem self_sub (a : R) : a - a = 0 :=
+
+theorem self_sub : a - a = 0 :=
 calc
   a - a = a + -a : by reflexivity
   ...   = 0      : by rw add_right_neg
