1678c1bcbf31cb22d220c8eff61db37437639793
Jose A. Alonso <JoseA.Alonso@gmail.com>
2020-07-06 08:18:54 +0200

    Táctica para retículos

--- src/Miscelaneas/Tactica_para_reticulos.lean
+++ src/Miscelaneas/Tactica_para_reticulos.lean
@@ -27,3 +27,29 @@ by lattice
 example : x ⊓ y = y ⊓ x :=
 by lattice
 
+-- 2ª solución
+-- ===========
+
+meta def tactic.interactive.lattice2 :=
+`[solve_by_elim
+  [le_antisymm, 
+   le_inf, 
+   sup_le, 
+   le_refl, 
+   inf_le_left_of_le, 
+   inf_le_right_of_le,
+   le_sup_left_of_le, 
+   le_sup_right_of_le]
+  {max_depth := 30}]
+
+example : a ⊔ (b ⊓ c) ≤ (a ⊔ b) ⊓ (a ⊔ c) :=
+by lattice2
+
+example : a ⊔ (a ⊓ b) = a :=
+by lattice2
+
+example : (x ⊓ y) ⊓ z = x ⊓ (y ⊓ z) :=
+by lattice2
+
+example : x ⊓ y = y ⊓ x :=
+by lattice2
