d22527ea830747a04be173bb44d29d059ee33c57
Jose A. Alonso <JoseA.Alonso@gmail.com>
2020-07-06 21:21:50 +0200

    Ejercicio con library_search

--- src/Basicos/Desigualdades_con_calc.lean
+++ src/Basicos/Desigualdades_con_calc.lean
@@ -1,24 +1,30 @@
-import data.real.basic tactic
+-- ---------------------------------------------------------------------
+-- Ejercicio. Sean a y b números reales. Demostrar que
+--    2*a*b ≤ a^2 + b^2
+-- ----------------------------------------------------------------------
+
+import data.real.basic 
+import tactic
 
 variables a b : ℝ
 
+-- 1ª demostración
 example : 2*a*b ≤ a^2 + b^2 :=
 begin
   have h : 0 ≤ a^2 - 2*a*b + b^2,
   calc
-    a^2 - 2*a*b + b^2 = (a - b)^2     : by ring
-    ... ≥ 0                           : by apply pow_two_nonneg,
+    a^2 - 2*a*b + b^2 = (a - b)^2       : by ring
+                  ... ≥ 0               : by apply pow_two_nonneg,
   calc
-    2*a*b
-        = 2*a*b + 0                   : by ring
-    ... ≤ 2*a*b + (a^2 - 2*a*b + b^2) : add_le_add (le_refl _) h
-    ... = a^2 + b^2                   : by ring
+    2*a*b = 2*a*b + 0                   : by ring
+      ... ≤ 2*a*b + (a^2 - 2*a*b + b^2) : add_le_add (le_refl _) h
+      ... = a^2 + b^2                   : by ring
 end
 
 -- 2ª demostración
 example : 2*a*b ≤ a^2 + b^2 :=
 begin
-  have h : 0 ≤ a^2 - 2*a*b + b^2,
+  have : 0 ≤ a^2 - 2*a*b + b^2,
   calc
     a^2 - 2*a*b + b^2 = (a - b)^2 : by ring
     ... ≥ 0                       : by apply pow_two_nonneg,
