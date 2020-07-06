c8a4efc09ef7f9dadf5cf7ed1643c2d5e961e795
Jose A. Alonso <JoseA.Alonso@gmail.com>
2020-07-06 12:52:15 +0200

    Propiedades del orden

--- src/Basicos/Propiedades_del_orden.lean
+++ src/Basicos/Propiedades_del_orden.lean
@@ -1,6 +1,22 @@
+-- ---------------------------------------------------------------------
+-- Ejercicio 1. Realizar las siguientes acciones:
+--    1. Importar la teoría de los números reales
+--    2. Declarar a, b y c como variables sobre R. 
+-- ----------------------------------------------------------------------
+
 import data.real.basic
 
 variables a b c : ℝ
+
+-- ---------------------------------------------------------------------
+-- Ejercicio 2. Calcular el tipo de las siguientes expresiones:
+--    + le_refl
+--    + le_trans
+--    + lt_of_le_of_lt
+--    + lt_of_lt_of_le
+--    + lt_trans
+-- ----------------------------------------------------------------------
+
 
 #check (le_refl  : ∀ a, a ≤ a)
 #check (le_trans : a ≤ b → b ≤ c → a ≤ c)
