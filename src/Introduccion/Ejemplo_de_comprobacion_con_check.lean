a7203c9ee0fdf51ee2b55021adf57f31dceeac46
Jose A. Alonso <JoseA.Alonso@gmail.com>
2022-08-18 10:54:16 +0200

    Completar la descripción de Ejemplo_de_comprobacion_con_check.lean

--- src/Introduccion/Ejemplo_de_comprobacion_con_check.lean
+++ src/Introduccion/Ejemplo_de_comprobacion_con_check.lean
@@ -4,6 +4,7 @@
 
 -- #check 2 + 3
 
--- Comentario: Al colocar el cursor sobre check escribe en el minibuffer
+-- Comentario: Al descomentar la línea anterior y colocar el cursor
+-- sobre check escribe en el minibuffer
 --    2 + 3 : ℕ
 -- que indica que el valor de la expresión es un número natural.
