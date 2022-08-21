f3dbcf75f02b327e0f4f966917b86e3b95f4f742
Jose A. Alonso <JoseA.Alonso@gmail.com>
2022-08-21 09:49:12 +0200

    Suprimir espacios en Asociativa_conmutativa_de_los_reales.lean

--- src/Basicos/Asociativa_conmutativa_de_los_reales.lean
+++ src/Basicos/Asociativa_conmutativa_de_los_reales.lean
@@ -1,6 +1,6 @@
 -- ---------------------------------------------------------------------
 -- Ejercicio. Demostrar que los números reales tienen la siguente
--- propiedad 
+-- propiedad
 --    (a * b) * c = b * (a * c)
 -- ---------------------------------------------------------------------
 
@@ -15,8 +15,8 @@ begin
   rw mul_assoc b a c,
 end
 
--- Comentarios: 
--- 1. Al colocar el cursor sobre el nombre de un lema se ve su enunciado. 
+-- Comentarios:
+-- 1. Al colocar el cursor sobre el nombre de un lema se ve su enunciado.
 -- 2. Para completar el nombre de un lema basta escribir parte de su
 --    nombre y completar con S-SPC (es decir, simultáneamente las teclas
 --    de mayúscula y la de espacio).
@@ -24,24 +24,24 @@ end
 --    + mul_com   : (∀ a b : G),   a * b = b * a
 --    + mul_assoc : (∀ a b c : G), (a * b) * c = a * (b * c)
 -- 4. La táctica (rw e) (ver https://bit.ly/3eUxbjD) reescribe una
---    expresión usando la ecuación e. 
+--    expresión usando la ecuación e.
 
 -- El desarrollo de la prueba es:
--- 
+--
 --    inicio
 --       a b c : ℝ
 --       ⊢ (a * b) * c = b * (a * c)
 --    rw mul_comm a b,
 --       a b c : ℝ
---       ⊢ (a * b) * c = b * (a * c)   
+--       ⊢ (a * b) * c = b * (a * c)
 --    rw mul_assoc b a c
 --       no goals
 
 -- 2ª demostración
 -- ===============
 
-example 
-  (a b c : ℝ) 
+example
+  (a b c : ℝ)
   : (a * b) * c = b * (a * c) :=
 calc
   (a * b) * c = (b * a) * c : by rw mul_comm a b
@@ -58,6 +58,3 @@ by ring
 
 -- Comentario: La táctica ring demuestra ecuaciones aplicando las
 -- propiedades de anillos.
-
-
-
