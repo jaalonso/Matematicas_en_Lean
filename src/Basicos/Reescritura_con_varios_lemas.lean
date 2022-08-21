d434682489378f7c8445c71f9a1ba099caea757c
Jose A. Alonso <JoseA.Alonso@gmail.com>
2022-08-21 13:38:47 +0200

    Suprimir blancos Reescritura_con_varios_lemas.lean

--- src/Basicos/Reescritura_con_varios_lemas.lean
+++ src/Basicos/Reescritura_con_varios_lemas.lean
@@ -1,6 +1,6 @@
 -- ---------------------------------------------------------------------
 -- Ejercicio. Demostrar que si a, b, c, d, e y f son números reales
--- tales que 
+-- tales que
 --    a * b = c * d
 --    e = f
 -- entonces
@@ -12,15 +12,15 @@ import data.real.basic
 -- 1ª demostración
 -- ===============
 
-example 
-  (a b c d e f : ℝ) 
-  (h1 : a * b = c * d) 
-  (h2 : e = f) 
+example
+  (a b c d e f : ℝ)
+  (h1 : a * b = c * d)
+  (h2 : e = f)
   : a * (b * e) = c * (d * f) :=
 begin
-  rw h2, 
-  rw ←mul_assoc, 
-  rw h1, 
+  rw h2,
+  rw ←mul_assoc,
+  rw h1,
   rw mul_assoc,
 end
 
@@ -30,22 +30,22 @@ end
 --    h1 : a * b = c * d,
 --    h2 : e = f
 --    ⊢ a * (b * e) = c * (d * f)
--- rw h2, 
+-- rw h2,
 --    ⊢ a * (b * f) = c * (d * f)
--- rw ←mul_assoc, 
+-- rw ←mul_assoc,
 --    ⊢ (a * b) * f = c * (d * f)
--- rw h1, 
+-- rw h1,
 --    ⊢ (c * d) * f = c * (d * f)
 -- rw mul_assoc,
---    no goals 
+--    no goals
 
 -- 2ª demostración
 -- ===============
 
-example 
-  (a b c d e f : ℝ) 
-  (h1 : a * b = c * d) 
-  (h2 : e = f) 
+example
+  (a b c d e f : ℝ)
+  (h1 : a * b = c * d)
+  (h2 : e = f)
   : a * (b * e) = c * (d * f) :=
 begin
   rw [h2, ←mul_assoc, h1, mul_assoc]
@@ -57,34 +57,33 @@ end
 -- 3ª demostración
 -- ===============
 
-example 
-  (a b c d e f : ℝ) 
-  (h1 : a * b = c * d) 
-  (h2 : e = f) 
+example
+  (a b c d e f : ℝ)
+  (h1 : a * b = c * d)
+  (h2 : e = f)
   : a * (b * e) = c * (d * f) :=
 by rw [h2, ←mul_assoc, h1, mul_assoc]
 
 -- 4ª demostración
 -- ===============
 
-example 
-  (a b c d e f : ℝ) 
-  (h1 : a * b = c * d) 
-  (h2 : e = f) 
+example
+  (a b c d e f : ℝ)
+  (h1 : a * b = c * d)
+  (h2 : e = f)
   : a * (b * e) = c * (d * f) :=
 by finish
 
 -- 5ª demostración
 -- ===============
 
-example 
-  (a b c d e f : ℝ) 
-  (h1 : a * b = c * d) 
-  (h2 : e = f) 
+example
+  (a b c d e f : ℝ)
+  (h1 : a * b = c * d)
+  (h2 : e = f)
   : a * (b * e) = c * (d * f) :=
 calc
-  a * (b * e) = a * (b * f) : by rw h2 
+  a * (b * e) = a * (b * f) : by rw h2
           ... = (a * b) * f : by rw ←mul_assoc
           ... = (c * d) * f : by rw h1
           ... = c * (d * f) : by rw mul_assoc
-
