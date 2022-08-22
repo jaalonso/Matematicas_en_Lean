80df0462c65083db473b65513c47bbf90aae3913
Jose A. Alonso <JoseA.Alonso@gmail.com>
2022-08-22 18:10:01 +0200

    Reordenar neg_add_cancel_left.lean

--- src/Basicos/neg_add_cancel_left.lean
+++ src/Basicos/neg_add_cancel_left.lean
@@ -1,5 +1,5 @@
 -- ---------------------------------------------------------------------
--- Ejercicio 1. Importar la teoría de anillos. 
+-- Ejercicio 1. Importar la teoría de anillos.
 -- ----------------------------------------------------------------------
 
 import algebra.ring
@@ -11,7 +11,7 @@ import algebra.ring
 namespace my_ring
 
 -- ---------------------------------------------------------------------
--- Ejercicio 3. Declarar R como una variable sobre anillos. 
+-- Ejercicio 3. Declarar R como una variable sobre anillos.
 -- ----------------------------------------------------------------------
 
 variables {R : Type*} [ring R]
@@ -25,36 +25,36 @@ variables {R : Type*} [ring R]
 -- ===============
 
 example
-  (a b : R) 
+  (a b : R)
   : -a + (a + b) = b :=
-calc
-  -a + (a + b) = (-a + a) + b : by rw ← add_assoc
-           ... = 0 + b        : by rw add_left_neg
-           ... = b            : by rw zero_add
+calc -a + (a + b)
+     = (-a + a) + b : by rw ← add_assoc
+ ... = 0 + b        : by rw add_left_neg
+ ... = b            : by rw zero_add
 
--- 2ª demostración 
+-- 2ª demostración
 -- ===============
 
-theorem neg_add_cancel_left 
-  (a b : R) 
+theorem neg_add_cancel_left
+  (a b : R)
   : -a + (a + b) = b :=
 by rw [←add_assoc, add_left_neg, zero_add]
 
 -- El desarrollo de la prueba es
--- 
+--
 --    R : Type u_1,
 --    _inst_1 : ring R,
 --    a b : R
 --    ⊢ -a + (a + b) = b
--- rw ← add_assoc, 
+-- rw ← add_assoc,
 --    ⊢ -a + a + b = b
--- rw add_left_neg, 
+-- rw add_left_neg,
 --    ⊢ 0 + b = b
 -- rw zero_add,
 --    no goals
 
 -- ---------------------------------------------------------------------
--- Ejercicio 6. Cerrar el espacio de nombre my_ring. 
+-- Ejercicio 6. Cerrar el espacio de nombre my_ring.
 -- ----------------------------------------------------------------------
 
 end my_ring
