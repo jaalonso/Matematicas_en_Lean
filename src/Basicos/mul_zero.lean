ec7426f8eb26118b1063bd76d8f59fe4912190fd
Jose A. Alonso <JoseA.Alonso@gmail.com>
2022-08-24 17:23:55 +0200

    Actualizar demostraciones en mul_zero.lean

--- src/Basicos/mul_zero.lean
+++ src/Basicos/mul_zero.lean
@@ -9,15 +9,18 @@ namespace my_ring
 
 variables {R : Type*} [ring R]
 
-variable (a : R) 
+variable (a : R)
 
 -- 1ª demostración
 -- ===============
 
-theorem mul_zero : a * 0 = 0 :=
+example : a * 0 = 0 :=
 begin
   have h : a * 0 + a * 0 = a * 0 + 0,
-  { rw [←mul_add, add_zero, add_zero] },
+    calc a * 0 + a * 0
+         = a * (0 + 0) : (mul_add a 0 0).symm
+     ... = a * 0       : congr_arg (λ x, a * x) (add_zero 0)
+     ... = a * 0 + 0   : (add_zero (a * 0)).symm,
   rw add_left_cancel h
 end
 
@@ -27,10 +30,34 @@ end
 example : a * 0 = 0 :=
 begin
   have h : a * 0 + a * 0 = a * 0 + 0,
-    calc a * 0 + a * 0 = a * (0 + 0) : by rw ←mul_add
-                   ... = a * 0       : by rw add_zero
-                   ... = a * 0 + 0   : by rw add_zero,
+    calc a * 0 + a * 0
+         = a * (0 + 0) : by rw ←mul_add
+     ... = a * 0       : by rw add_zero
+     ... = a * 0 + 0   : by rw add_zero,
   rw add_left_cancel h
+end
+
+-- 3ª demostración
+-- ===============
+
+example : a * 0 = 0 :=
+begin
+  have h : a * 0 + a * 0 = a * 0 + 0,
+  { rw [←mul_add, add_zero, add_zero] },
+  rw add_left_cancel h
+end
+
+-- 4ª demostración
+-- ===============
+
+example : a * 0 = 0 :=
+begin
+  have h : a * 0 + a * 0 = a * 0 + 0,
+    calc a * 0 + a * 0
+         = a * (0 + 0) : by simp
+     ... = a * 0       : by simp
+     ... = a * 0 + 0   : by simp,
+  simp,
 end
 
 end my_ring
