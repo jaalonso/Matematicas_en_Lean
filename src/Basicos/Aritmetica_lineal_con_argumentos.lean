92683dffbdea4dfe730e95cbd2ecdb0eb3533a46
Jose A. Alonso <JoseA.Alonso@gmail.com>
2022-09-01 18:24:35 +0200

    Cambiar importación en Aritmetica_lineal_con_argumentos.lean

--- src/Basicos/Aritmetica_lineal_con_argumentos.lean
+++ src/Basicos/Aritmetica_lineal_con_argumentos.lean
@@ -1,20 +1,19 @@
 -- ---------------------------------------------------------------------
 -- Ejercicio. Sean a, b, y c números reales. Demostrar  que si
---    1 ≤ a 
+--    1 ≤ a
 --    b ≤ c
 -- entonces
 --    2 + a + exp b ≤ 3 * a + exp c
 -- ----------------------------------------------------------------------
 
-
-import analysis.special_functions.exp_log
+import analysis.special_functions.log.basic
 
 open real
 
 variables a b c : ℝ
 
-example 
-  (h  : 1 ≤ a) 
-  (h' : b ≤ c) 
+example
+  (h  : 1 ≤ a)
+  (h' : b ≤ c)
   : 2 + a + exp b ≤ 3 * a + exp c :=
 by linarith [exp_le_exp.mpr h']
