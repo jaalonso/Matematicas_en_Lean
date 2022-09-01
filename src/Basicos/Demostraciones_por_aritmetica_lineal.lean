1b487bbf5b44ddee5e646699b22231a4eb0eacda
Jose A. Alonso <JoseA.Alonso@gmail.com>
2022-09-01 18:19:24 +0200

    Borrar espacios en Demostraciones_por_aritmetica_lineal.lean

--- src/Basicos/Demostraciones_por_aritmetica_lineal.lean
+++ src/Basicos/Demostraciones_por_aritmetica_lineal.lean
@@ -1,7 +1,7 @@
 -- ---------------------------------------------------------------------
 -- Ejercicio 1. Sean a, b, c, d y e números reales. Demostrar que si
---    a ≤ b 
---    b < c 
+--    a ≤ b
+--    b < c
 --    c ≤ d
 --    d < e
 -- entonces
@@ -13,8 +13,8 @@ import data.real.basic
 variables a b c d e : ℝ
 
 example
-  (h₀ : a ≤ b) 
-  (h₁ : b < c) 
+  (h₀ : a ≤ b)
+  (h₁ : b < c)
   (h₂ : c ≤ d)
   (h₃ : d < e)
   : a < e :=
@@ -24,14 +24,14 @@ by linarith
 -- Ejercicio 2. Demostrar que si
 --    2 * a ≤ 3 * b
 --    1 ≤ a
---    d = 2 
+--    d = 2
 -- entonces
 --    d + a ≤ 5 * b
 -- ----------------------------------------------------------------------
 
-example 
-  (h   : 2 * a ≤ 3 * b) 
-  (h'  : 1 ≤ a) 
-  (h'' : d = 2) 
+example
+  (h   : 2 * a ≤ 3 * b)
+  (h'  : 1 ≤ a)
+  (h'' : d = 2)
   : d + a ≤ 5 * b :=
 by linarith
