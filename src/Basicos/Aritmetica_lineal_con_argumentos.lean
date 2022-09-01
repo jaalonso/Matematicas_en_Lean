755a2a61c8a695f352b3007f3ea9c0674b6bb9d0
Jose A. Alonso <JoseA.Alonso@gmail.com>
2022-09-01 18:31:34 +0200

    Cambiar variables en Aritmetica_lineal_con_argumentos.lean

--- src/Basicos/Aritmetica_lineal_con_argumentos.lean
+++ src/Basicos/Aritmetica_lineal_con_argumentos.lean
@@ -1,19 +1,19 @@
 -- ---------------------------------------------------------------------
--- Ejercicio. Sean a, b, y c números reales. Demostrar  que si
+-- Ejercicio. Sean a, b, y d números reales. Demostrar  que si
 --    1 ≤ a
---    b ≤ c
+--    b ≤ d
 -- entonces
---    2 + a + exp b ≤ 3 * a + exp c
+--    2 + a + exp b ≤ 3 * a + exp d
 -- ----------------------------------------------------------------------
 
 import analysis.special_functions.log.basic
 
 open real
 
-variables a b c : ℝ
+variables a b d : ℝ
 
 example
   (h  : 1 ≤ a)
-  (h' : b ≤ c)
-  : 2 + a + exp b ≤ 3 * a + exp c :=
+  (h' : b ≤ d)
+  : 2 + a + exp b ≤ 3 * a + exp d :=
 by linarith [exp_le_exp.mpr h']
