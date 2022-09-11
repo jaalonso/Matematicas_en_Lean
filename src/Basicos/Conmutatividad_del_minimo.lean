24b4165cc701d153d98459687802999fb4af4b7b
Jose A. Alonso <JoseA.Alonso@gmail.com>
2022-09-11 12:58:13 +0200

    Ampliar demostraciones de src/Basicos/Conmutatividad_del_minimo.lean

--- src/Basicos/Conmutatividad_del_minimo.lean
+++ src/Basicos/Conmutatividad_del_minimo.lean
@@ -16,11 +16,11 @@ begin
   { show min a b ≤ min b a,
     apply le_min,
     { apply min_le_right },
-    apply min_le_left },
+    { apply min_le_left }},
   { show min b a ≤ min a b,
     apply le_min,
     { apply min_le_right },
-    apply min_le_left }
+    { apply min_le_left }},
 end
 
 -- Nota: Se usa "show" para indicar lo que se demuestra en cada bloque.
@@ -48,18 +48,31 @@ end
 
 example : min a b = min b a :=
 begin
-  have h : ∀ x y, min x y ≤ min y x,
+  have h : ∀ x y : ℝ, min x y ≤ min y x,
   { intros x y,
     apply le_min,
-    apply min_le_right,
-    apply min_le_left },
-  apply le_antisymm, apply h, apply h
+    { apply min_le_right },
+    { apply min_le_left }},
+  apply le_antisymm,
+  apply h,
+  apply h,
 end
 
 -- Nota: La táctica "intros" introduce las variables en el contexto. Ver
 -- https://bit.ly/2UF1EdL
 
--- 3ª demostración (con repeat)
+-- 3ª demostración
+-- ===============
+
+example : min a b = min b a :=
+begin
+  have h : ∀ {x y : ℝ}, min x y ≤ min y x,
+  { intros x y,
+    exact le_min (min_le_right x y) (min_le_left x y) },
+  exact le_antisymm h h,
+end
+
+-- 4ª demostración (con repeat)
 -- ============================
 
 example : min a b = min b a :=
