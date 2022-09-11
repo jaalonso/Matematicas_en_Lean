7533a297d85e2505ea83a65a52b1a8cfb4ce21e0
Jose A. Alonso <JoseA.Alonso@gmail.com>
2022-09-11 11:33:49 +0200

    Ampliar demostraciones en src/Basicos/Desigualdades_con_calc.lean

--- src/Basicos/Desigualdades_con_calc.lean
+++ src/Basicos/Desigualdades_con_calc.lean
@@ -3,7 +3,7 @@
 --    2*a*b ≤ a^2 + b^2
 -- ----------------------------------------------------------------------
 
-import data.real.basic 
+import data.real.basic
 import tactic
 
 variables a b : ℝ
@@ -11,22 +11,35 @@ variables a b : ℝ
 -- 1ª demostración
 example : 2*a*b ≤ a^2 + b^2 :=
 begin
-  have h : 0 ≤ a^2 - 2*a*b + b^2,
-  calc
-    a^2 - 2*a*b + b^2 = (a - b)^2       : by ring
-                  ... ≥ 0               : by apply pow_two_nonneg,
-  calc
-    2*a*b = 2*a*b + 0                   : by ring
-      ... ≤ 2*a*b + (a^2 - 2*a*b + b^2) : add_le_add (le_refl _) h
-      ... = a^2 + b^2                   : by ring
+  have : 0 ≤ (a - b)^2 := pow_two_nonneg (a - b),
+  have : 0 ≤ a^2 - 2*a*b + b^2, by linarith,
+  show 2*a*b ≤ a^2 + b^2, by linarith,
 end
 
 -- 2ª demostración
 example : 2*a*b ≤ a^2 + b^2 :=
 begin
+  have h : 0 ≤ a^2 - 2*a*b + b^2,
+  { calc a^2 - 2*a*b + b^2
+        = (a - b)^2                   : by ring
+    ... ≥ 0                           : by apply pow_two_nonneg },
+  calc 2*a*b
+       = 2*a*b + 0                   : by ring
+   ... ≤ 2*a*b + (a^2 - 2*a*b + b^2) : add_le_add (le_refl _) h
+   ... = a^2 + b^2                   : by ring
+end
+
+-- 3ª demostración
+example : 2*a*b ≤ a^2 + b^2 :=
+begin
   have : 0 ≤ a^2 - 2*a*b + b^2,
-  calc
-    a^2 - 2*a*b + b^2 = (a - b)^2 : by ring
-    ... ≥ 0                       : by apply pow_two_nonneg,
+  { calc a^2 - 2*a*b + b^2
+         = (a - b)^2       : by ring
+     ... ≥ 0               : by apply pow_two_nonneg },
   linarith,
 end
+
+-- 4ª demostración
+example : 2*a*b ≤ a^2 + b^2 :=
+-- by library_search
+two_mul_le_add_sq a b
