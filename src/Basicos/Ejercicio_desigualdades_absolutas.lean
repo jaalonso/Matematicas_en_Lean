f8951e600b326ddea5e96890ecef04af7f61a5c7
Jose A. Alonso <JoseA.Alonso@gmail.com>
2022-09-11 12:21:05 +0200

    Ampliar demostraciones de src/Basicos/Ejercicio_desigualdades_absolutas.lean

--- src/Basicos/Ejercicio_desigualdades_absolutas.lean
+++ src/Basicos/Ejercicio_desigualdades_absolutas.lean
@@ -1,6 +1,6 @@
 -- ---------------------------------------------------------------------
 -- Ejercicio. Sean a y b números reales. Demostrar que
---    abs (a*b) ≤ (a^2 + b^2) / 2 :=
+--    abs (a*b) ≤ (a^2 + b^2) / 2
 -- ----------------------------------------------------------------------
 
 import data.real.basic
@@ -8,26 +8,58 @@ import tactic
 
 variables a b : ℝ
 
-
+-- 1ª demostración
 example : abs (a*b) ≤ (a^2 + b^2) / 2 :=
 begin
   apply abs_le.mpr,
   split,
-  { have h3 : 0 ≤ a^2 + 2*a*b + b^2,
+  { have h1 : 0 ≤ a^2 + 2*a*b + b^2,
       calc 0 ≤ (a+b)^2                : by exact pow_two_nonneg (a + b)
          ... = a^2+2*a*b+b^2          : by ring,
-    have h4 : -2*(a*b) ≤ a^2 + b^2,
+    have h2 : -2*(a*b) ≤ a^2 + b^2,
       calc -2*(a*b)
-           ≤ -2*(a*b)+(a^2+2*a*b+b^2) : by exact le_add_of_nonneg_right h3
+           ≤ -2*(a*b)+(a^2+2*a*b+b^2) : by exact le_add_of_nonneg_right h1
        ... = a^2 + b^2                : by ring,
-    by linarith [h4] },
-  { have h1 : 0 ≤ a^2 - 2*a*b + b^2,
+    show -((a^2 + b^2) / 2) ≤ a*b,      by linarith [h2] },
+  { have h3 : 0 ≤ a^2 - 2*a*b + b^2,
       calc 0 ≤ (a-b)^2                : by exact pow_two_nonneg (a - b)
          ... = a^2-2*a*b+b^2          : by ring,
-    have h2 : 2*(a*b) ≤ a^2 + b^2,
+    have h4 : 2*(a*b) ≤ a^2 + b^2,
       calc 2*(a*b)
-           ≤ 2*(a*b)+(a^2-2*a*b+b^2)  : by exact le_add_of_nonneg_right h1
+           ≤ 2*(a*b)+(a^2-2*a*b+b^2)  : by exact le_add_of_nonneg_right h3
        ... = a^2 + b^2                : by ring,
-    by linarith [h2] },
-    exact ordered_add_comm_monoid.to_covariant_class_left ℝ,
+    show a * b ≤ (a^2 + b^2)/2,         by linarith [h4] },
+end
+
+-- 2ª demostración
+example : abs (a*b) ≤ (a^2 + b^2) / 2 :=
+begin
+  apply abs_le.mpr,
+  split,
+  { have h1 : 0 ≤ a^2 + 2*a*b + b^2,
+      calc 0 ≤ (a+b)^2                : by exact pow_two_nonneg (a + b)
+         ... = a^2+2*a*b+b^2          : by ring,
+    have h2 : -2*(a*b) ≤ a^2 + b^2,
+      calc -2*(a*b)
+           ≤ -2*(a*b)+(a^2+2*a*b+b^2) : by exact le_add_of_nonneg_right h1
+       ... = a^2 + b^2                : by ring,
+    show -((a^2 + b^2) / 2) ≤ a*b,      by linarith [h2] },
+  { have h4 : 2*a*b ≤ a^2 + b^2       := two_mul_le_add_sq a b,
+    show a * b ≤ (a^2 + b^2)/2,         by linarith [h4] },
+end
+
+-- 3ª demostración
+example : abs (a*b) ≤ (a^2 + b^2) / 2 :=
+begin
+  apply abs_le.mpr,
+  split,
+  { have h1 : 0 ≤ a^2 + 2*a*b + b^2,
+      calc 0 ≤ (a+b)^2                : by exact pow_two_nonneg (a + b)
+         ... = a^2+2*a*b+b^2          : by ring,
+    have h2 : -2*(a*b) ≤ a^2 + b^2,
+      calc -2*(a*b)
+           ≤ -2*(a*b)+(a^2+2*a*b+b^2) : by exact le_add_of_nonneg_right h1
+       ... = a^2 + b^2                : by ring,
+    show -((a^2 + b^2) / 2) ≤ a*b,      by linarith [h2] },
+  { show a * b ≤ (a^2 + b^2)/2,         by linarith [two_mul_le_add_sq a b] },
 end
