9d209d9caa364e1e1a9cbdba5004ffa69db175bc
Jose A. Alonso <JoseA.Alonso@gmail.com>
2022-09-26 17:12:44 +0200

    Cambiar demostración en src/Basicos/abs_sub.lean

--- src/Basicos/abs_sub.lean
+++ src/Basicos/abs_sub.lean
@@ -1,22 +1,14 @@
 -- ---------------------------------------------------------------------
 -- Ejercicio. Sean a y b números reales. Demostrar que
---    abs a - abs b ≤ abs (a - b)
+--    |a| - |b| ≤ |a - b|
 -- ----------------------------------------------------------------------
 
 import data.real.basic
 
 variables a b c : ℝ
 
-example : abs a - abs b ≤ abs (a - b) :=
-begin
-  apply sub_le_iff_le_add.mpr,
-  have h1 : abs a = abs ((a - b) + b),
-    by ring_nf,
-  rw h1,
-  apply abs_add,
-  exact covariant_swap_add_le_of_covariant_add_le ℝ,
-end
-
--- Lemas usados:
--- #check (abs_add : ∀ a b : ℝ, abs (a + b) ≤ abs a + abs b)
--- #check (sub_le_iff_le_add : a - b ≤ c ↔ a ≤ c + b)
+example : |a| - |b| ≤ |a - b| :=
+calc |a| - |b|
+     = |a - b + b| - |b|     : by simp
+ ... ≤ (|a - b| + |b|) - |b| : sub_le_sub_right (abs_add (a - b) b) (|b|)
+ ... = |a - b|               : add_sub_cancel (|a - b|) (|b|)
