42a53a9ba1c6e386298a408aee4ea76e51997894
Jose A. Alonso <JoseA.Alonso@gmail.com>
2022-10-18 18:34:08 +0200

    Actualizar demostraciones

--- src/Basicos/Asociatividad_del_infimo.lean
+++ src/Basicos/Asociatividad_del_infimo.lean
@@ -13,6 +13,43 @@ variables x y z : α
 
 example : (x ⊓ y) ⊓ z = x ⊓ (y ⊓ z) :=
 begin
+  have h1 : (x ⊓ y) ⊓ z ≤ x ⊓ (y ⊓ z),
+    { have h1a : (x ⊓ y) ⊓ z ≤ x, calc
+        (x ⊓ y) ⊓ z ≤ x ⊓ y : by exact inf_le_left
+                ... ≤ x     : by exact inf_le_left,
+      have h1b : (x ⊓ y) ⊓ z ≤ y ⊓ z,
+        { have h1b1 : (x ⊓ y) ⊓ z ≤ y, calc
+            (x ⊓ y) ⊓ z ≤ x ⊓ y : by exact inf_le_left
+                    ... ≤ y     : by exact inf_le_right,
+          have h1b2 : (x ⊓ y) ⊓ z ≤ z,
+            by exact inf_le_right,
+          show (x ⊓ y) ⊓ z ≤ y ⊓ z,
+            by exact le_inf h1b1 h1b2, },
+      show (x ⊓ y) ⊓ z ≤ x ⊓ (y ⊓ z),
+        by exact le_inf h1a h1b, },
+  have h2 : x ⊓ (y ⊓ z) ≤ (x ⊓ y) ⊓ z,
+    { have h2a : x ⊓ (y ⊓ z) ≤ x ⊓ y,
+        { have h2a1 : x ⊓ (y ⊓ z) ≤ x,
+            by exact inf_le_left,
+          have h2a2 : x ⊓ (y ⊓ z) ≤ y, calc
+            x ⊓ (y ⊓ z) ≤ y ⊓ z : by exact inf_le_right
+                    ... ≤ y     : by exact inf_le_left,
+          show x ⊓ (y ⊓ z) ≤ x ⊓ y,
+            by exact le_inf h2a1 h2a2, },
+      have h2b : x ⊓ (y ⊓ z) ≤ z, calc
+        x ⊓ (y ⊓ z) ≤ y ⊓ z : by exact inf_le_right
+                ... ≤ z     : by exact inf_le_right,
+      show x ⊓ (y ⊓ z) ≤ (x ⊓ y) ⊓ z,
+        by exact le_inf h2a h2b, },
+  show (x ⊓ y) ⊓ z = x ⊓ (y ⊓ z),
+    by exact le_antisymm h1 h2,
+end
+
+-- 2ª demostración
+-- ===============
+
+example : (x ⊓ y) ⊓ z = x ⊓ (y ⊓ z) :=
+begin
   apply le_antisymm,
   { apply le_inf,
     { apply inf_le_of_left_le inf_le_left, },
@@ -40,7 +77,7 @@ end
 --      { apply inf_le_right_of_le inf_le_right, },},
 -- no goals
 
--- 2ª demostración
+-- 3ª demostración
 -- ===============
 
 example : (x ⊓ y) ⊓ z = x ⊓ (y ⊓ z) :=
@@ -52,8 +89,16 @@ le_antisymm
     (le_inf inf_le_left (inf_le_of_right_le inf_le_left))
     (inf_le_of_right_le inf_le_right))
 
--- 3ª demostración
+-- 4ª demostración
 -- ===============
 
 example : (x ⊓ y) ⊓ z = x ⊓ (y ⊓ z) :=
+-- by library_search
 inf_assoc
+
+-- 5ª demostración
+-- ===============
+
+example : (x ⊓ y) ⊓ z = x ⊓ (y ⊓ z) :=
+-- by hint
+by finish
