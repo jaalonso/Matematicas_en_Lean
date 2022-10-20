7d89275b27b210bef13438053f8c7b3fbed3cbbb
Jose A. Alonso <JoseA.Alonso@gmail.com>
2022-10-20 09:03:51 +0200

    Asociatividad del máximo

--- src/Basicos/Asociatividad_del_supremo.lean
+++ src/Basicos/Asociatividad_del_supremo.lean
@@ -13,6 +13,62 @@ variables x y z : α
 
 example : (x ⊔ y) ⊔ z = x ⊔ (y ⊔ z) :=
 begin
+  have h1 : (x ⊔ y) ⊔ z ≤ x ⊔ (y ⊔ z),
+    { have h1a : x ⊔ y ≤ x ⊔ (y ⊔ z), by finish,
+      have h1b : z ≤ x ⊔ (y ⊔ z), by finish,
+      show (x ⊔ y) ⊔ z ≤ x ⊔ (y ⊔ z),
+        by exact sup_le h1a h1b, },
+  have h2 : x ⊔ (y ⊔ z) ≤ (x ⊔ y) ⊔ z,
+    { have h2a : x ≤ (x ⊔ y) ⊔ z, by finish,
+      have h2b : y ⊔ z ≤ (x ⊔ y) ⊔ z, by finish,
+      show x ⊔ (y ⊔ z) ≤ (x ⊔ y) ⊔ z,
+        by exact sup_le h2a h2b, },
+  show (x ⊔ y) ⊔ z = x ⊔ (y ⊔ z),
+    by exact le_antisymm h1 h2,
+end
+
+-- 2ª demostración
+-- ===============
+
+example : (x ⊔ y) ⊔ z = x ⊔ (y ⊔ z) :=
+begin
+  have h1 : (x ⊔ y) ⊔ z ≤ x ⊔ (y ⊔ z),
+    { have h1a : x ⊔ y ≤ x ⊔ (y ⊔ z),
+        { have h1a1 : x ≤ x ⊔ (y ⊔ z) :=
+            le_sup_left,
+          have h1a2 : y ≤ x ⊔ (y ⊔ z), calc
+            y ≤ y ⊔ z         : le_sup_left
+            ... ≤ x ⊔ (y ⊔ z) : le_sup_right,
+          show x ⊔ y ≤ x ⊔ (y ⊔ z),
+            by exact sup_le h1a1 h1a2, },
+      have h1b : z ≤ x ⊔ (y ⊔ z), calc
+        z   ≤ y ⊔ z       : le_sup_right
+        ... ≤ x ⊔ (y ⊔ z) : le_sup_right,
+      show (x ⊔ y) ⊔ z ≤ x ⊔ (y ⊔ z),
+        by exact sup_le h1a h1b, },
+  have h2 : x ⊔ (y ⊔ z) ≤ (x ⊔ y) ⊔ z,
+    { have h2a : x ≤ (x ⊔ y) ⊔ z, calc
+        x   ≤ x ⊔ y       : le_sup_left
+        ... ≤ (x ⊔ y) ⊔ z : le_sup_left,
+      have h2b : y ⊔ z ≤ (x ⊔ y) ⊔ z,
+        { have h2b1 : y ≤ (x ⊔ y) ⊔ z, calc
+            y   ≤ x ⊔ y       : le_sup_right
+            ... ≤ (x ⊔ y) ⊔ z : le_sup_left,
+          have h2b2 : z ≤ (x ⊔ y) ⊔ z :=
+            le_sup_right,
+          show y ⊔ z ≤ (x ⊔ y) ⊔ z,
+            by exact sup_le h2b1 h2b2, },
+      show x ⊔ (y ⊔ z) ≤ (x ⊔ y) ⊔ z,
+        by exact sup_le h2a h2b, },
+  show (x ⊔ y) ⊔ z = x ⊔ (y ⊔ z),
+    by exact le_antisymm h1 h2,
+end
+
+-- 3ª demostración
+-- ===============
+
+example : (x ⊔ y) ⊔ z = x ⊔ (y ⊔ z) :=
+begin
   apply le_antisymm,
   { apply sup_le,
     { apply sup_le le_sup_left (le_sup_of_le_right le_sup_left)},
@@ -40,7 +96,7 @@ end
 -- | |      { apply sup_le (le_sup_left_of_le le_sup_right) le_sup_right}},
 -- no goals
 
--- 2ª demostración
+-- 4ª demostración
 -- ===============
 
 example : (x ⊔ y) ⊔ z = x ⊔ (y ⊔ z) :=
@@ -52,8 +108,16 @@ le_antisymm
     (le_sup_of_le_left le_sup_left)
     (sup_le (le_sup_of_le_left le_sup_right) le_sup_right))
 
--- 3ª demostración
+-- 5ª demostración
 -- ===============
 
 example : x ⊔ y ⊔ z = x ⊔ (y ⊔ z) :=
+-- by library_search
 sup_assoc
+
+-- 6ª demostración
+-- ===============
+
+example : x ⊔ y ⊔ z = x ⊔ (y ⊔ z) :=
+-- by hint
+by finish
