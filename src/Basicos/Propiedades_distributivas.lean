5338377a02ed0a0730c0bae54aff1350e627f645
Jose A. Alonso <JoseA.Alonso@gmail.com>
2022-10-21 11:44:47 +0200

    Actualizar demostraciones de Basicos/Propiedades_distributivas.lean

--- src/Basicos/Propiedades_distributivas.lean
+++ src/Basicos/Propiedades_distributivas.lean
@@ -2,28 +2,28 @@
 -- Ejercicio 1. Realizar las siguientes acciones
 --    1. Importar la teoría de retículos.
 --    2. Declarar α como un tipo sobre retículos
---    3. Declarar a, b y c como variabkes sobre α 
+--    3. Declarar a, b y c como variabkes sobre α
 -- ----------------------------------------------------------------------
 
 import order.lattice               -- 1
 variables {α : Type*} [lattice α]  -- 2
-variables a b c : α                  -- 3
+variables a b c : α                -- 3
 
 -- ---------------------------------------------------------------------
 -- Ejercicio 2. Demostrar que si
 --    ∀ x y z : α, x ⊓ (y ⊔ z) = (x ⊓ y) ⊔ (x ⊓ z))
 -- entonces
---    (a ⊔ b) ⊓ c = (a ⊓ c) ⊔ (b ⊓ c) 
+--    (a ⊔ b) ⊓ c = (a ⊓ c) ⊔ (b ⊓ c)
 -- ----------------------------------------------------------------------
 
-example 
-  (h : ∀ x y z : α, x ⊓ (y ⊔ z) = (x ⊓ y) ⊔ (x ⊓ z)) 
+example
+  (h : ∀ x y z : α, x ⊓ (y ⊔ z) = (x ⊓ y) ⊔ (x ⊓ z))
   : (a ⊔ b) ⊓ c = (a ⊓ c) ⊔ (b ⊓ c) :=
 calc
   (a ⊔ b) ⊓ c = c ⊓ (a ⊔ b)       : by rw inf_comm
           ... = (c ⊓ a) ⊔ (c ⊓ b) : by rw h
-          ... = (a ⊓ c) ⊔ (c ⊓ b) : by rw [@inf_comm _ _ c a]  
-          ... = (a ⊓ c) ⊔ (b ⊓ c) : by rw [@inf_comm _ _ c b]  
+          ... = (a ⊓ c) ⊔ (c ⊓ b) : by rw [@inf_comm _ _ c a]
+          ... = (a ⊓ c) ⊔ (b ⊓ c) : by rw [@inf_comm _ _ c b]
 
 -- ---------------------------------------------------------------------
 -- Ejercicio 3. Demostrar que si
@@ -32,12 +32,11 @@ calc
 --    (a ⊓ b) ⊔ c = (a ⊔ c) ⊓ (b ⊔ c)
 -- ----------------------------------------------------------------------
 
-example 
-  (h : ∀ x y z : α, x ⊔ (y ⊓ z) = (x ⊔ y) ⊓ (x ⊔ z)) 
+example
+  (h : ∀ x y z : α, x ⊔ (y ⊓ z) = (x ⊔ y) ⊓ (x ⊔ z))
   : (a ⊓ b) ⊔ c = (a ⊔ c) ⊓ (b ⊔ c) :=
 calc
   (a ⊓ b) ⊔ c = c ⊔ (a ⊓ b)       : by rw sup_comm
           ... = (c ⊔ a) ⊓ (c ⊔ b) : by rw h
-          ... = (a ⊔ c) ⊓ (c ⊔ b) : by rw [@sup_comm _ _ c a]  
-          ... = (a ⊔ c) ⊓ (b ⊔ c) : by rw [@sup_comm _ _ c b]  
-
+          ... = (a ⊔ c) ⊓ (c ⊔ b) : by rw [@sup_comm _ _ c a]
+          ... = (a ⊔ c) ⊓ (b ⊔ c) : by rw [@sup_comm _ _ c b]
