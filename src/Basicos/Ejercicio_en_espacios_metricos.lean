1ef7ea49daddbc6e6251700c2ab7436e9d8beaf3
Jose A. Alonso <JoseA.Alonso@gmail.com>
2022-10-26 10:49:07 +0200

    Modificar Basicos/Ejercicio_en_espacios_metricos.lean

--- src/Basicos/Ejercicio_en_espacios_metricos.lean
+++ src/Basicos/Ejercicio_en_espacios_metricos.lean
@@ -11,14 +11,15 @@ variables x y : X
 
 -- 1ª demostración
 example : 0 ≤ dist x y :=
-  have 2 * dist x y ≥ 0,
-    from calc
-      2 * dist x y = dist x y + dist x y : by rw two_mul
-               ... = dist x y + dist y x : by rw [dist_comm x y]
-               ... ≥ dist x x            : by apply dist_triangle
-               ... = 0                   : by rw ← dist_self x,
-  nonneg_of_mul_nonneg_left this zero_lt_two
+  have h1 : 2 * dist x y ≥ 0, by calc
+    2 * dist x y = dist x y + dist x y : two_mul (dist x y)
+             ... = dist x y + dist y x : by rw [dist_comm x y]
+             ... ≥ dist x x            : dist_triangle x y x
+             ... = 0                   : dist_self x,
+  show 0 ≤ dist x y,
+    by exact nonneg_of_mul_nonneg_left h1 zero_lt_two
 
 -- 2ª demostración
 example : 0 ≤ dist x y :=
+-- by library_search
 dist_nonneg
