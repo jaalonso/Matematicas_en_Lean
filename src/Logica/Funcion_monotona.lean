1328268cbca6b98f0c5e3264b0e13e4c2c3b3ef0
Jose A. Alonso <JoseA.Alonso@gmail.com>
2022-11-08 18:20:16 +0100

    Actualizar Logica/Funcion_monotona.lean

--- src/Logica/Funcion_monotona.lean
+++ src/Logica/Funcion_monotona.lean
@@ -1,11 +1,11 @@
 -- ---------------------------------------------------------------------
 -- Ejercicio. Explicitar la definición de función monótona poniendo el
--- nombre en la hipótesis y su definición en la conclusión. 
+-- nombre en la hipótesis y su definición en la conclusión.
 -- ----------------------------------------------------------------------
 
 import data.real.basic
 
-example 
-  (f : ℝ → ℝ) 
+example
+  (f : ℝ → ℝ)
   (h : monotone f) :
   ∀ {a b}, a ≤ b → f a ≤ f b := h
