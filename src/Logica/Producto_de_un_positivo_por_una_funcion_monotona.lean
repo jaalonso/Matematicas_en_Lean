b4a24d3cdee70fc18a08e29a712fcf02c9b57270
Jose A. Alonso <JoseA.Alonso@gmail.com>
2022-11-09 17:10:49 +0100

    Actualizar Logica/Producto_de_un_positivo_por_una_funcion_monotona.lean

--- src/Logica/Producto_de_un_positivo_por_una_funcion_monotona.lean
+++ src/Logica/Producto_de_un_positivo_por_una_funcion_monotona.lean
@@ -6,44 +6,61 @@
 import data.real.basic
 
 variables (f : ℝ → ℝ)
+variable  {c : ℝ}
 
 -- 1ª demostración
 -- ===============
 
-example 
-  {c : ℝ}       
-  (mf : monotone f) 
-  (nnc : 0 ≤ c) 
+example
+  (mf : monotone f)
+  (nnc : 0 ≤ c)
   : monotone (λ x, c * f x) :=
-begin 
-  intros a b aleb,
+begin
+  have h1 : ∀ a b, a ≤ b → (λ x, c * f x) a ≤ (λ x, c * f x) b,
+    { intros a b hab,
+      have h2 : f a ≤ f b := mf hab,
+      have h3 : c * f a ≤ c * f b := mul_le_mul_of_nonneg_left h2 nnc,
+      show (λ x, c * f x) a ≤ (λ x, c * f x) b,
+        by exact h3, },
+  show monotone (λ x, c * f x),
+    by exact h1,
+end
+
+-- 2ª demostración
+-- ===============
+
+example
+  (mf : monotone f)
+  (nnc : 0 ≤ c)
+  : monotone (λ x, c * f x) :=
+begin
+  intros a b hab,
   apply mul_le_mul_of_nonneg_left,
-  apply mf aleb,
+  apply mf hab,
   apply nnc
 end
 
 -- Su desarrollo es
--- 
+--
 -- f : ℝ → ℝ,
 -- c : ℝ,
 -- mf : monotone f,
 -- nnc : 0 ≤ c
 -- ⊢ monotone (λ (x : ℝ), c * f x)
---    >> intros a b aleb,
+--    >> intros a b hab,
 -- a b : ℝ,
--- aleb : a ≤ b
+-- hab : a ≤ b
 -- ⊢ (λ (x : ℝ), c * f x) a ≤ (λ (x : ℝ), c * f x) b
 --    >> apply mul_le_mul_of_nonneg_left,
 -- | ⊢ f a ≤ f b
--- |    >> apply mf aleb,
+-- |    >> apply mf hab,
 -- | ⊢ 0 ≤ c
 -- |    >> apply nnc
 -- no goals
 
--- 2ª demostración
+-- 3ª demostración
 -- ===============
 
-example {c : ℝ} (mf : monotone f) (nnc : 0 ≤ c) :
+example (mf : monotone f) (nnc : 0 ≤ c) :
   monotone (λ x, c * f x) :=
-λ a b aleb, mul_le_mul_of_nonneg_left (mf aleb) nnc
-
+λ a b hab, mul_le_mul_of_nonneg_left (mf hab) nnc
