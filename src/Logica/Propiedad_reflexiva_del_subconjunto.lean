674527fde7d7d48c11ec216e55ab17638b6192fc
Jose A. Alonso <JoseA.Alonso@gmail.com>
2022-11-17 20:19:37 +0100

    Actualizar Propiedad_reflexiva_del_subconjunto.lean

--- src/Logica/Propiedad_reflexiva_del_subconjunto.lean
+++ src/Logica/Propiedad_reflexiva_del_subconjunto.lean
@@ -11,12 +11,37 @@ variables {α : Type*} (s : set α)
 
 example : s ⊆ s :=
 begin
-  intros x xs,
-  exact xs,
+  assume x,
+  assume xs: x ∈ s,
+  show x ∈ s,
+    by exact xs,
 end
 
 -- 2ª demostración
 -- ===============
 
 example : s ⊆ s :=
+begin
+  intros x xs,
+  exact xs,
+end
+
+-- 3ª demostración
+-- ===============
+
+example : s ⊆ s :=
 λ x (xs : x ∈ s), xs
+
+-- 4ª demostración
+-- ===============
+
+example : s ⊆ s :=
+-- by library_search
+rfl.subset
+
+-- 5ª demostración
+-- ===============
+
+example : s ⊆ s :=
+-- by hint
+by refl
