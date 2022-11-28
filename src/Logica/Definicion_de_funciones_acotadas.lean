e9545ed0b11e2f3e0162faeeb87dd462a09f8724
Jose A. Alonso <JoseA.Alonso@gmail.com>
2022-11-28 18:37:39 +0100

    Quitar rspacios en Definicion_de_funciones_acotadas

--- src/Logica/Definicion_de_funciones_acotadas.lean
+++ src/Logica/Definicion_de_funciones_acotadas.lean
@@ -1,7 +1,7 @@
 import data.real.basic
 
 -- ---------------------------------------------------------------------
--- Ejercicio 1. Definir la función 
+-- Ejercicio 1. Definir la función
 --    fn_ub (ℝ → ℝ) → ℝ → Prop
 -- tal que (fn_ub f a) afirma que a es una cota superior de f.
 -- ----------------------------------------------------------------------
@@ -9,7 +9,7 @@ import data.real.basic
 def fn_ub (f : ℝ → ℝ) (a : ℝ) : Prop := ∀ x, f x ≤ a
 
 -- ---------------------------------------------------------------------
--- Ejercicio 2. Definir la función 
+-- Ejercicio 2. Definir la función
 --    fn_lb (ℝ → ℝ) → ℝ → Prop
 -- tal que (fn_lb f a) afirma que a es una cota inferior de f.
 -- ----------------------------------------------------------------------
@@ -17,7 +17,7 @@ def fn_ub (f : ℝ → ℝ) (a : ℝ) : Prop := ∀ x, f x ≤ a
 def fn_lb (f : ℝ → ℝ) (a : ℝ) : Prop := ∀ x, a ≤ f x
 
 -- ---------------------------------------------------------------------
--- Ejercicio 3. Definir la función 
+-- Ejercicio 3. Definir la función
 --    fn_has_ub (ℝ → ℝ) → Prop
 -- tal que (fn_ub f) afirma que f tiene cota superior.
 -- ----------------------------------------------------------------------
@@ -25,7 +25,7 @@ def fn_lb (f : ℝ → ℝ) (a : ℝ) : Prop := ∀ x, a ≤ f x
 def fn_has_ub (f : ℝ → ℝ) := ∃ a, fn_ub f a
 
 -- ---------------------------------------------------------------------
--- Ejercicio 4. Definir la función 
+-- Ejercicio 4. Definir la función
 --    fn_has_lb (ℝ → ℝ) → Prop
 -- tal que (fn_lb f) afirma que f tiene cota inferior.
 -- ----------------------------------------------------------------------
