a81929fccbaccfc4378f1176751781be45532f7a
Jose A. Alonso <JoseA.Alonso@gmail.com>
2022-11-29 20:03:39 +0100

    Ampliar Suma_de_funciones_acotadas_inferiormente.lean

--- src/Logica/Suma_de_funciones_acotadas_inferiormente.lean
+++ src/Logica/Suma_de_funciones_acotadas_inferiormente.lean
@@ -1,7 +1,7 @@
 -- ---------------------------------------------------------------------
 -- Ejercicio 1. Realizar las siguientes acciones:
 -- 1. Importar la teoría Definicion_de_funciones_acotadas
--- 2. Declarar f y g como variables de funciones de ℝ en ℝ. 
+-- 2. Declarar f y g como variables de funciones de ℝ en ℝ.
 -- 3. Declarar a y b como variables sobre ℝ.
 -- ----------------------------------------------------------------------
 
@@ -18,10 +18,10 @@ variables {a b : ℝ}                      -- 3
 -- 1ª demostración
 -- ===============
 
-lemma fn_lb_add 
-  (hfa : fn_lb f a) 
-  (hgb : fn_lb g b) 
-  : fn_lb (λ x, f x + g x) (a + b) :=
+lemma fn_lb_add
+  (hfa : fn_lb f a)
+  (hgb : fn_lb g b)
+  : fn_lb (f + g) (a + b) :=
 begin
   intro x,
   change a + b ≤ f x + g x,
@@ -31,7 +31,7 @@ begin
 end
 
 -- Su desarrollo es
--- 
+--
 -- f g : ℝ → ℝ,
 -- a b : ℝ,
 -- hfa : fn_lb f a,
@@ -53,9 +53,9 @@ end
 -- ===============
 
 example
-  (hfa : fn_lb f a) 
-  (hgb : fn_lb g b) 
-  : fn_lb (λ x, f x + g x) (a + b) :=
+  (hfa : fn_lb f a)
+  (hgb : fn_lb g b)
+  : fn_lb (f + g) (a + b) :=
 λ x, add_le_add (hfa x) (hgb x)
 
 -- ---------------------------------------------------------------------
@@ -63,10 +63,30 @@ example
 -- inferiormente también lo está.
 -- ----------------------------------------------------------------------
 
-example 
-  (lbf : fn_has_lb f) 
-  (lbg : fn_has_lb g) 
-  : fn_has_lb (λ x, f x + g x) :=
+-- 1ª demostración
+-- ===============
+
+example
+  (lbf : fn_has_lb f)
+  (lbg : fn_has_lb g)
+  : fn_has_lb (f + g) :=
+begin
+  cases lbf with a ha,
+  cases lbg with b hb,
+  have h1 : fn_lb (f + g) (a + b) := fn_lb_add ha hb,
+  have h2 : ∃ z, ∀ x, z ≤ (f + g) x :=
+    by exact Exists.intro (a + b) h1,
+  show fn_has_lb (f + g),
+    by exact h2,
+end
+
+-- 2ª demostración
+-- ===============
+
+example
+  (lbf : fn_has_lb f)
+  (lbg : fn_has_lb g)
+  : fn_has_lb (f + g) :=
 begin
   cases lbf with a lbfa,
   cases lbg with b lbgb,
@@ -75,7 +95,7 @@ begin
 end
 
 -- Su desarrollo es
--- 
+--
 -- f g : ℝ → ℝ,
 -- lbf : fn_has_lb f,
 -- lbg : fn_has_lb g
@@ -97,3 +117,33 @@ end
 -- ⊢ fn_lb (λ (x : ℝ), f x + g x) (a + b)
 --    >> apply fn_lb_add lbfa lbgb
 -- no goals
+
+-- 3ª demostración
+-- ===============
+
+example
+  (lbf : fn_has_lb f)
+  (lbg : fn_has_lb g)
+  : fn_has_lb (f + g) :=
+begin
+  rcases lbf with ⟨a, lbfa⟩,
+  rcases lbg with ⟨b, lbfb⟩,
+  exact ⟨a + b, fn_lb_add lbfa lbfb⟩,
+end
+
+-- 4ª demostración
+-- ===============
+
+example :
+  fn_has_lb f → fn_has_lb g → fn_has_lb (f + g) :=
+begin
+  rintros ⟨a, lbfa⟩ ⟨b, lbfb⟩,
+  exact ⟨a + b, fn_lb_add lbfa lbfb⟩,
+end
+
+-- 5ª demostración
+-- ===============
+
+example :
+  fn_has_lb f → fn_has_lb g → fn_has_lb (f + g) :=
+λ ⟨a, lbfa⟩ ⟨b, lbfb⟩, ⟨a + b, fn_lb_add lbfa lbfb⟩
