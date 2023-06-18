d788a7bf7380c7d4f839e40fb4c3d45cd1bc6039
removedporn <86824510+removedporn@users.noreply.github.com>
2023-06-19 04:47:12 +0800

    Delete src directory

--- src/Introduccion/Ejemplo_de_evaluacion.lean
+++
@@ -1,8 +0,0 @@
--- ---------------------------------------------------------------------
--- Ejercicio. Calcular el valor de 2+3.
--- ---------------------------------------------------------------------
-
--- #eval 2 + 3
-
--- Comentario: Al descomentar la línea anterior y poner el cursor sobre
--- eval se escribe el resultado en el minibuffer.
